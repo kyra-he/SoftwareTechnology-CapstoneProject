c34847dac0d342a695cdd6da50a5d28f6b1fd8b3
Kyra <166001057+kyrawrrr@users.noreply.github.com>
2024-04-28 20:28:29 +1000

    Add files via upload

---
+++ Deployment.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import tensorflow as tf
+from PIL import Image, ImageOps
+import numpy as np
+from streamlit_option_menu import option_menu
+
+st.write("Hello world")
+
+streamlit.run.Deployment.py
